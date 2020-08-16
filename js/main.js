1b7b2b7fb671dd348c3586b0404a9055c6041518
Csokibacsi <csokibacsi1@gmail.com>
2020-08-16 11:55:15 +0200

    szerveradatok táblázatba írása

--- js/main.js
+++ js/main.js
@@ -5,13 +5,49 @@ function getServerData(url){
         cache: "no-cache"
     };
 
-    fetch(url, fetchOptions).then(
+    return fetch(url, fetchOptions).then(
         response => response.json(),
         err=> console.error(err)
-    ).then(
-        data => console.log(data)
+  
     );
 }
+
+document.querySelector("#getDataBtn").addEventListener("click",function() {
+    getServerData("http://localhost:3000/users").then(
+        //data => console.log(data)
+        data => fillDataTable(data, "userTable")
+    );
+});
+
+//fill table with data
+function fillDataTable(data, tableID){
+    let table = document.querySelector(`#${tableID}`);
+    if(!table){
+        console.error(`Table "${tableID}" is not found.`);
+        return;
+    }
+
+    let tBody = table.querySelector("tbody");
+    for (let row of data){
+        //console.log(row);
+        let tr = createAnyElement("tr");
+        for(let k in row){
+            let td = createAnyElement("td");
+            td.innerHTML = row[k];
+            tr.appendChild(td);
+        }
+        tBody.appendChild(tr);
+    }
+}
+
+function createAnyElement(name, attributes){
+    let element = document.createElement(name);
+    for(let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
 
 getServerData("http://localhost:3000/users").then(
     data => console.log(data)
